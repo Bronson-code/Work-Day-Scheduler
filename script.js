5cb8be25485d882a82dfe6a70a7fc136d6d27e6f
Bronson-code <oliverbronson@yahoo.com>
2020-08-15 18:04:42 -0700

    Completed application. Functional Javascript, updated functions and variables.

--- script.js
+++ script.js
@@ -24,3 +24,62 @@ $(document).ready(function(){
             }
         })
     });
+    // Create object where each key is a time of day.
+    // Every value, this object starts as an empty string.
+    var hourObj = {
+        "09": "",
+        "10": "",
+        "11": "",
+        "12": "", 
+        "13": "",
+        "14": "",
+        "15": "",
+        "16": "",
+        "17": "",
+    }
+
+
+    // When save button is clicked, contents of text area are saved to local storage.
+    
+    $(".saveBtn").on("click", function() {
+        // Save text area content to a variable.
+        // Connect save button and textarea so that information can be passed into function.
+        var rowHour = $(this).parent().attr("data-hour");
+        var value = $(this).siblings("textarea").val();
+
+        // console.log(rowHour, value);
+    // Ex: hourObj = {9am: "Wake Up", 10am: "", 11am: "Make Breakfast"...}
+    // var value = "make coffee"
+    var savedData = JSON.parse(localStorage.getItem("hourObj"));
+    if (savedData !== null){
+        savedData[rowHour] = value
+        localStorage.setItem("hourObj", JSON.stringify(savedData) );
+        
+    } else {
+        hourObj[rowHour] = value
+        localStorage.setItem("hourObj", JSON.stringify(hourObj));
+    }
+
+
+    })
+
+    var savedData = JSON.parse(localStorage.getItem("hourObj"));
+    
+
+    $(".time-block .col-md-10").each(function(index, value){
+    // console.log(value);
+    console.log(index);
+    
+        if (0 === index){
+            console.log(savedData["09"]);
+            $(value).val(savedData["09"])
+        } else {
+            var temp = index;
+            temp = temp + 9;
+            console.log(toString(temp));
+            console.log(savedData[toString(temp)]);
+            $(value).val(savedData[`${temp}`]);
+        
+        }
+
+    }) 
